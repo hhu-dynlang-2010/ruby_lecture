4c24bbd01058455f7e7acee3531f7987a1e8f8dd
David Schneider <david.schneider@picle.org>
2010-05-02 11:50:07 +0200

    add slides about open class and move vector example there

---
+++ classes_and_methods/src/vector.rb
@@ -0,0 +1,55 @@
+class Vector
+  def initialize(values)
+    @values = values
+  end
+
+  def *(other)
+    Vector.new(@values.map{|v| v * other})
+  end
+
+  def values()
+    @values
+  end
+end
+
+class Fixnum
+  alias mult *
+  def *(other)
+    if other.is_a?(Vector)
+      other * self
+    else
+      self.mult(other)
+    end
+  end
+end
+
+describe Vector do
+  subject {Vector.new([1,2,3])}
+
+  its(:values) { should == [1,2,3] }
+
+  describe 'scalar multiplication' do
+    it 'should multiply with the scalar as right operand' do
+      r = subject * 3
+      r.values().should == [3, 6, 9]
+    end
+
+    it 'should multiply with the scaler as left operand' do
+      r = 4 * subject
+      r.values().should == [4, 8, 12]
+    end
+  end
+end
+
+describe Fixnum do
+
+  it 'should multiply numbers as expected' do
+    (3 * 7).should == 21
+  end
+
+  describe '.mult' do
+    it 'behave as *' do
+      3.mult(7).should == 21
+    end
+  end
+end
